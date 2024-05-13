2e2d9bcef9ca09db0bf511fe0afe8f0327873fa6
caihong <caihong@163.com>
2024-05-14 07:42:58 +0800

    增加JacksonConfig

---
+++ src/main/java/com/learn/more/config/JacksonConfig.java
@@ -0,0 +1,20 @@
+package com.learn.more.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@Configuration
+public class JacksonConfig {
+
+  @Bean
+  public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+    CustomDateSerializer dateSerializer = new CustomDateSerializer();
+    ObjectMapper mapper = builder.createXmlMapper(false).serializers(dateSerializer).failOnEmptyBeans(false).failOnUnknownProperties(false).build();
+    mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    return mapper;
+  }
+}
