edd506e9dc991ec62ba961df47e10ca42112050f
侯凯旗 <alan.hou@pkfare.com>
2024-06-01 17:24:36 +0800

    feat: 增加测试

--- src/main/java/com/learn/more/config/JacksonConfig.java
+++ src/main/java/com/learn/more/config/JacksonConfig.java
@@ -2,6 +2,8 @@ package com.learn.more.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -17,7 +19,9 @@ public class JacksonConfig {
         .serializers(dateSerializer)
         .failOnEmptyBeans(false)
         .failOnUnknownProperties(false)
+        .modules(new JavaTimeModule(), new Jdk8Module())
         .build();
+    // 格式化输出
     mapper.enable(SerializationFeature.INDENT_OUTPUT);
     mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     return mapper;
